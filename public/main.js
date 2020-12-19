a76accf2acda5220785092958e163bb96c53b2d0
HAFIZH <73621103+HAFizh-15@users.noreply.github.com>
2020-12-19 07:07:22 +0700

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,18 @@
+//make connection
+var socket = window.socket = io.connect(location.origin)
+
+socket.on('memoryUsage', e => {
+	console.log('Receive:', e)
+	let list = []
+	let div = document.querySelector('div#usage')
+	for (key in e) list.push(`<pre><code>${key}: ${e[key]}</code></pre>`)
+	div.innerHTML = list.join('')
+})
+
+setInterval(socket.emit, 2500, 'memoryUsage')
+/*
+document.addEventListener('keypress', (e) => {
+	socket.emit('0', e.key)
+	console.log('Send:', e.key)
+})
+*/
